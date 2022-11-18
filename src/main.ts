d27681716b214c096ee3bcdefd733969134eaf6c
Nivendha <knivendha@gmail.com>
2022-11-18 18:47:52 +0000

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
-import './index.css'
+import Typed,{TypedOptions} from "typed.js";
+import "./index.css";
 
+
+const typedElem = new Typed(".typed", {
+  strings: ["I am <br/> frontend  <br/>  devloper", "I am  <br/>  backend  <br/> devloper"],
+  typeSpeed: 30,
+  backSpeed: 20,
+  smartBackspace: true,
+  showCursor: false,
+  autoInsertCss: true,
+  loop:true
+} as TypedOptions);
