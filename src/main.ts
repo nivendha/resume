85e5372db2cc989959ef80cd039eb5a2ed5f0efc
Nivendha <knivendha@gmail.com>
2022-11-30 15:26:59 +0000

    animate added

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,55 @@
-import Typed,{TypedOptions} from "typed.js";
+import Typed, { TypedOptions } from "typed.js";
+import anime from "animejs/lib/anime.es.js";
+
 import "./index.css";
 
+window.onload = function () {
+  let mainTimeline = anime.timeline({
+    easing: "easeOutExpo",
+    // duration: 750,
+  });
 
-const typedElem = new Typed(".typed", {
-  strings: ["I am <br/> frontend  <br/>  devloper", "I am  <br/>  backend  <br/> devloper"],
-  typeSpeed: 30,
-  backSpeed: 20,
-  smartBackspace: true,
-  showCursor: false,
-  autoInsertCss: true,
-  loop:true
-} as TypedOptions);
+  mainTimeline
+    .add({
+      targets: ".triangle",
+      borderRightWidth: [0, "100vw"],
+    })
+    .add(
+      {
+        targets: ".left-content .circle",
+        width: "50vw",
+      },
+      "-=900"
+    )
+    .add(
+      {
+        targets: [".left-content .avatar", ".left-content .avatar-arc"],
+        scale: [0, 1],
+      },
+      "-=900"
+    )
+    .add(
+      {
+        targets: ".arc-content",
+        opacity: [0, 1],
+      },
+      "-=900"
+    );
+
+  mainTimeline.complete = () => {
+    new Typed(".typed", {
+      strings: [
+        "I am <br/> frontend  <br/>  devloper",
+        "I am  <br/>  backend  <br/> devloper",
+      ],
+      typeSpeed: 30,
+      backSpeed: 20,
+      smartBackspace: true,
+      showCursor: false,
+      autoInsertCss: true,
+      loop: true,
+    } as TypedOptions);
+  };
+};
+
+// mainTimeline.play()
